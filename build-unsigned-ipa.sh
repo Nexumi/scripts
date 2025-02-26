89db807f512ece8b4deaba3cb0b52d49b064099a
Jimmy Pan <48866805+Nexumi@users.noreply.github.com>
2025-02-26 12:06:25 -0800

    Update build-unsigned-ipa.sh

--- build-unsigned-ipa.sh
+++ build-unsigned-ipa.sh
@@ -1,6 +1,6 @@
-PROJECT=""
+PROJECT=$(find . -type f -name "*.xcodeproj" -print -quit | xargs -I {} basename {} .xcodeproj)
 
-xcodebuild archive -project ${PROJECT}.xcodeproj -scheme ${PROJECT_NAME} -archivePath unsigned.xcarchive -configuration Release CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO
+xcodebuild archive -project ${PROJECT}.xcodeproj -scheme ${PROJECT} -archivePath unsigned.xcarchive -configuration Release CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO
 mv unsigned.xcarchive/Products/Applications .
 rm -rf unsigned.xcarchive
 mv Applications Payload
