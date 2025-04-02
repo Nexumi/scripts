d4fd9090ac987ed2d757de27f7b62a586356ec30
Jimmy Pan <48866805+Nexumi@users.noreply.github.com>
2025-04-02 10:47:03 -0700

    Update build-unsigned-ipa.sh

--- build-unsigned-ipa.sh
+++ build-unsigned-ipa.sh
@@ -1,8 +1,16 @@
 PROJECT=$(find . -type d -name "*.xcodeproj" -print -quit | xargs -I {} basename {} .xcodeproj)
 
-xcodebuild archive -project ${PROJECT}.xcodeproj -scheme ${PROJECT} -archivePath unsigned.xcarchive -configuration Release CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO
-mv unsigned.xcarchive/Products/Applications .
-rm -rf unsigned.xcarchive
-mv Applications Payload
-zip -r App.ipa Payload
-rm -rf Payload
+if xcodebuild archive -project "${PROJECT}.xcodeproj" -scheme "${PROJECT}" -archivePath unsigned.xcarchive -configuration Release CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO; then
+    echo "xcodebuild succeeded. Proceeding with packaging."
+
+    mv unsigned.xcarchive/Products/Applications .
+    rm -rf unsigned.xcarchive
+    mv Applications Payload
+    zip -r App.ipa Payload
+    rm -rf Payload
+
+    echo "App.ipa successfully created."
+else
+    echo "xcodebuild failed. Exiting."
+    exit 1
+fi
